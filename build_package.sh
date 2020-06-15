d1c2b13ee98a4ec93b381c52d0468a10758da95b
Joan <jom@grosjo.net>
2020-06-15 14:36:23 +0100

    Archlinux addition

--- build_package.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-checkinstall --pkgname=tomboy-reborn --maintainer="\"Joan Moreau <jom@grosjo.net>\""  --requires=libcanberra-gtk-module --pkggroup=x11 --pkgversion=1.0
--- packages/create_deb.sh
+++ packages/create_deb.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "DEBIAN"
 checkinstall --pkgname=tomboy-reborn --maintainer="\"Joan Moreau <jom@grosjo.net>\""  --requires=libcanberra-gtk-module --pkggroup=x11 --pkgversion=1.0 --pkgsource="https://github.com/grosjo/tomboy-reborn" --pkgrelease=beta1
+echo "RPM"
 alien -r -c -v tomboy-reborn_1.0-beta1_amd64.deb
+echo "ARCH"
+debtap tomboy-reborn_1.0-beta1_amd64.deb
 
