84cc4f10cc1054fbbe054c2fe960769393fc35cc
Joan <jom@grosjo.net>
2020-06-15 16:12:02 +0300

    Add RPM generation

--- build_package/DEBIAN/create_deb.sh
+++ build_package/DEBIAN/create_deb.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 checkinstall --pkgname=tomboy-reborn --maintainer="\"Joan Moreau <jom@grosjo.net>\""  --requires=libcanberra-gtk-module --pkggroup=x11 --pkgversion=1.0 --pkgsource="https://github.com/grosjo/tomboy-reborn" --pkgrelease=beta1
+alien -r -c -v tomboy-reborn_1.0-beta1_amd64.deb
+
