1186ce8e2d8e19d0af240b1b518c065fd410183d
Joan <jom@grosjo.net>
2020-06-15 16:10:17 +0300

    Deb building

--- build_package/DEBIAN/create_deb.sh
+++ build_package/DEBIAN/create_deb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-checkinstall --pkgname=tomboy-reborn --maintainer="\"Joan Moreau <jom@grosjo.net>\""  --requires=libcanberra-gtk-module --pkggroup=x11 --pkgversion=1.0
+checkinstall --pkgname=tomboy-reborn --maintainer="\"Joan Moreau <jom@grosjo.net>\""  --requires=libcanberra-gtk-module --pkggroup=x11 --pkgversion=1.0 --pkgsource="https://github.com/grosjo/tomboy-reborn" --pkgrelease=beta1
