b144a79af83601adb9fbde5e7cbda91cdbf1fcd0
Joan Moreau <jom@grosjo.net>
2020-06-16 20:41:28 +0100

    Beta2

--- packages/create_deb.sh
+++ packages/create_deb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "DEBIAN"
-checkinstall --pkgname=tomboy-reborn --maintainer="\"Joan Moreau <jom@grosjo.net>\""  --requires=libcanberra-gtk-module --pkggroup=x11 --pkgversion=1.0 --pkgsource="https://github.com/grosjo/tomboy-reborn" --pkgrelease=beta1
+checkinstall --pkgname=tomboy-reborn --maintainer="\"Joan Moreau <jom@grosjo.net>\""  --requires=libcanberra-gtk-module --pkggroup=x11 --pkgversion=1.0 --pkgsource="https://github.com/grosjo/tomboy-reborn" --pkgrelease=beta2
 echo "RPM"
-alien -r -c -v tomboy-reborn_1.0-beta1_amd64.deb
+alien -r -c -v tomboy-reborn_1.0-beta2_amd64.deb
 echo "ARCH"
-debtap tomboy-reborn_1.0-beta1_amd64.deb
+debtap tomboy-reborn_1.0-beta2_amd64.deb
 
